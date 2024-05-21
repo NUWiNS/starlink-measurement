3d6b33d4a36cd33bce6501a5eae129afa879932f
brrruski <brrruski@suiet.app>
2024-05-21 15:48:14 -0400

    feat: upgrade dependencies

--- install_deps.sh
+++ install_deps.sh
@@ -5,7 +5,12 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
     apt install $dependencies
 elif [[ "$OSTYPE" == "linux-android"* ]]; then
     pkg update
-    pkg install $dependencies
+
+    # setup external storage in termux
+    pkg install termux-am 
+    termux-setup-storage
+
+    pkg install $dependencies 
     # check grpcurl version: https://github.com/fullstorydev/grpcurl/releases
     wget https://github.com/fullstorydev/grpcurl/releases/download/v1.9.1/grpcurl_1.9.1_linux_arm64.tar.gz
     tar -xvf grpcurl_1.9.1_linux_arm64.tar.gz
