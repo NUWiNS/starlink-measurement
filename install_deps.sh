ccba44d297a763d9300bce6c105669454f8238b7
brrruski <brrruski@suiet.app>
2024-05-21 11:32:15 -0400

    feat: update install deps script

--- install_deps.sh
+++ install_deps.sh
@@ -1,10 +1,16 @@
 # Install dependencies
-dependencies="grpcurl jq"
+dependencies="wget jq"
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
     apt install $dependencies
 elif [[ "$OSTYPE" == "linux-android"* ]]; then
+    pkg update
     pkg install $dependencies
+    # check grpcurl version: https://github.com/fullstorydev/grpcurl/releases
+    wget https://github.com/fullstorydev/grpcurl/releases/download/v1.9.1/grpcurl_1.9.1_linux_arm64.tar.gz
+    tar -xvf grpcurl_1.9.1_linux_arm64.tar.gz
+    chmod +x grpcurl
+    mv grpcurl /data/data/com.termux/files/usr/bin/
 elif [[ "$OSTYPE" == "darwin"* ]]; then
     brew install $dependencies
 else
