0ebefcae128fe0a797b5e85d9da9d7180cfdfcea
brrruski <brrruski@suiet.app>
2024-05-23 10:02:00 -0400

    tmp: test child process terminiation

---
+++ master.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Start the subscript in the background
+./subscript.sh &
+SUBSCRIPT_PID=$!
+
+kill_subscript() {
+    echo "Master script caught signal, terminating subscript..."
+    kill $SUBSCRIPT_PID
+    wait $SUBSCRIPT_PID
+    echo "Subscript terminated."
+    exit 0
+}
+
+# Function to handle cleanup on exit
+cleanup() {
+    echo "Master script caught signal, terminating subscript..."
+    kill $SUBSCRIPT_PID
+    wait $SUBSCRIPT_PID
+    echo "Subscript terminated."
+    exit 0
+}
+
+# Set up trap to catch termination signals
+trap cleanup SIGINT SIGTERM SIGHUP
+
+# Simulate some work in the master script
+while true; do
+    echo "Master script doing some work..."
+    sleep 3
+    kill_subscript
+done
