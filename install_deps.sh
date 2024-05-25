edc2263b70311d9e96f4d70bc1bc639f66058700
brrruski <brrruski@suiet.app>
2024-05-25 16:19:48 -0400

    feat: update deps script

--- install_deps.sh
+++ install_deps.sh
@@ -9,21 +9,35 @@ elif [[ "$OSTYPE" == "linux-android"* ]]; then
     pkg update
 
     # setup external storage in termux
-    pkg install termux-am 
-    termux-setup-storage
+    if [! command -v termux-setup-storage &> /dev/null]; then
+        pkg install termux-am 
+        termux-setup-storage
+    fi
 
-    pkg install $dependencies 
+    pkg install $dependencies
     
     usr_bin_path=/data/data/com.termux/files/usr/bin
 
-    # check grpcurl version: https://github.com/fullstorydev/grpcurl/releases
-    chmod +x grpcurl
-    cp ./deps/grpcurl $usr_bin_path/grpcurl
-    chmod +x $usr_bin_path/grpcurl
+    if [! command -v grpcurl &> /dev/null]; then
+        # check grpcurl version: https://github.com/fullstorydev/grpcurl/releases
+        chmod +x grpcurl
+        cp ./deps/grpcurl $usr_bin_path/grpcurl
+        chmod +x $usr_bin_path/grpcurl
+        echo "grpcurl installed in $usr_bin_path"
+    fi
 
     # nuttcp: https://nuttcp.net/nuttcp/
-    cp ./deps/nuttcp-8.1.4 $usr_bin_path/nuttcp
-    chmod +x $usr_bin_path/nuttcp
+    if [! command -v nuttcp &> /dev/null]; then
+        cp ./deps/nuttcp-8.1.4 $usr_bin_path/nuttcp
+        chmod +x $usr_bin_path/nuttcp
+        echo "nuttcp installed in $usr_bin_path"
+    fi
+
+    if [! command -v iperf3 &> /dev/null]; then
+        cp ./deps/iperf3.9 $usr_bin_path/iperf3
+        chmod +x $usr_bin_path/iperf3
+        echo "iperf3 installed in $usr_bin_path"
+    fi
 
 elif [[ "$OSTYPE" == "darwin"* ]]; then
     brew install $dependencies
