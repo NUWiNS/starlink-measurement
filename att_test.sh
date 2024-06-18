3cf2a6804664774ac1dfa55cec28b69cb0d96c62
brrruski <brrruski@suiet.app>
2024-06-18 10:48:41 -0400

    chore: update IP address of the testing server

--- att_test.sh
+++ att_test.sh
@@ -2,7 +2,8 @@
 
 # 1 for loop, 2 for one-shot mode
 MODE=2
-SERVER=35.245.244.238
+#SERVER=35.245.244.238 # Virginia gcloud server
+SERVER=50.112.93.113 # Oregon aws server
 # 1 for Verizon, 2 for ATT, 3 for Starlink
 OPERATOR=2
 
