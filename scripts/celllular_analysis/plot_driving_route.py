eef8d6fc79ca75834f8d965cfa0fecf1358ffde6
sizhewang <wang.sizh@northeastern.edu>
2024-10-23 16:22:22 -0400

    feat: add drive route from xcal data for hawaii

--- scripts/celllular_analysis/plot_driving_route.py
+++ scripts/celllular_analysis/plot_driving_route.py
@@ -1,20 +1,23 @@
+import glob
+from typing import List
 import pandas as pd
 import folium
 import os
 
 import sys
 
+
 CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.join(CURRENT_DIR, '../..'))
 OUTPUT_DIR = os.path.join(CURRENT_DIR, "outputs")
 
 from scripts.alaska_starlink_trip.configs import ROOT_DIR as ALASKA_ROOT_DIR
+from scripts.hawaii_starlink_trip.configs import ROOT_DIR as HAWAII_ROOT_DIR
+from scripts.constants import DATASET_DIR
 
-
-def plot_driving_route(df: pd.DataFrame, output_file_path: str):
+def plot_driving_route(df: pd.DataFrame, center_coordinates: List[float], output_file_path: str):
     # Create a map centered on Alaska
-    alaska_center = [61.2181, -149.9003]  # Anchorage coordinates
-    m = folium.Map(location=alaska_center, zoom_start=6)
+    m = folium.Map(location=center_coordinates, zoom_start=6)
 
     # Create a PolyLine of the driving route
     route_coordinates = df[['latitude', 'longitude']].values.tolist()
@@ -32,18 +35,109 @@ def plot_driving_route(df: pd.DataFrame, output_file_path: str):
 
     print(f"Map saved as '{output_file_path}'")
 
+def plot_route_in_alaska_with_all_xcal_data():
+    alaska_xcal_files = glob.glob(os.path.join(DATASET_DIR, 'xcal', '*_ALASKA_*.xlsx'))
+    if len(alaska_xcal_files) == 0:
+        raise ValueError("No xcal files found in the dataset directory")
+    # merge the Lat and Lon cols among all hawaii xcal files
+    df_alaska_gps_data_in_alaska = pd.DataFrame({
+        "latitude": [],
+        "longitude": []
+    })
+    for alaska_xcal_file in alaska_xcal_files:
+        df = pd.read_excel(alaska_xcal_file)
+        df = df[["Lat", "Lon"]].dropna()
+        df = df.drop_duplicates(subset=["Lat", "Lon"])  # Remove duplicate lat/lon pairs
+        
+        # Filter out coordinates not related to Alaska
+        # alaska_lat_range = (60.5, 62.5)  # Approximate latitude range for Alaska
+        # alaska_lon_range = (-150.5, -148.5)  # Approximate longitude range for Alaska
+        # df = df[(df["Lat"].between(*alaska_lat_range)) & (df["Lon"].between(*alaska_lon_range))]
+        
+        df_alaska_gps_data_in_alaska = pd.concat([df_alaska_gps_data_in_alaska, pd.DataFrame({
+            "latitude": df["Lat"],
+            "longitude": df["Lon"]
+        })], ignore_index=True)
+        df_alaska_gps_data_in_alaska = df_alaska_gps_data_in_alaska.drop_duplicates(subset=["latitude", "longitude"])  # Remove duplicate lat/lon pairs after concatenation
+        
+    output_alaska_driving_route_file_path = os.path.join(OUTPUT_DIR, "alaska_driving_route_with_all_xcal_data.html")
+    plot_driving_route(
+        df_alaska_gps_data_in_alaska, 
+        center_coordinates=[61.2181, -149.9003],  # Anchorage coordinates
+        output_file_path=output_alaska_driving_route_file_path
+    )
+
+def plot_route_in_hawaii_with_all_xcal_data():
+    hawaii_xcal_files = glob.glob(os.path.join(DATASET_DIR, 'xcal', '*_HAWAII_*.xlsx'))
+    if len(hawaii_xcal_files) == 0:
+        raise ValueError("No xcal files found in the dataset directory")
+    # merge the Lat and Lon cols among all hawaii xcal files
+    df_hawaii_gps_data_in_hawaii = pd.DataFrame({
+        "latitude": [],
+        "longitude": []
+    })
+    for hawaii_xcal_file in hawaii_xcal_files:
+        df = pd.read_excel(hawaii_xcal_file)
+        df = df[["Lat", "Lon"]].dropna()
+        df = df.drop_duplicates(subset=["Lat", "Lon"])  # Remove duplicate lat/lon pairs
+        
+        # Filter out coordinates not related to Hawaii
+        hawaii_lat_range = (18.5, 22.5)  # Approximate latitude range for Hawaii
+        hawaii_lon_range = (-160.5, -154.5)  # Approximate longitude range for Hawaii
+        df = df[(df["Lat"].between(*hawaii_lat_range)) & (df["Lon"].between(*hawaii_lon_range))]
+        
+        df_hawaii_gps_data_in_hawaii = pd.concat([df_hawaii_gps_data_in_hawaii, pd.DataFrame({
+            "latitude": df["Lat"],
+            "longitude": df["Lon"]
+        })], ignore_index=True)
+        df_hawaii_gps_data_in_hawaii = df_hawaii_gps_data_in_hawaii.drop_duplicates(subset=["latitude", "longitude"])  # Remove duplicate lat/lon pairs after concatenation
+        
+    output_hawaii_driving_route_file_path = os.path.join(OUTPUT_DIR, "hawaii_driving_route_with_all_xcal_data.html")
+    plot_driving_route(
+        df_hawaii_gps_data_in_hawaii, 
+        center_coordinates=[20.7943, -156.3319],  # Maui coordinates
+        output_file_path=output_hawaii_driving_route_file_path
+    )
+
+
+
+def plot_route_in_alaska_within_measurements():
+    df_att_xcal_data_in_alaska = pd.read_csv(os.path.join(ALASKA_ROOT_DIR, "xcal", "att_smart_tput.csv"))
+    df_att_gps_data_in_alaska = pd.DataFrame({
+        "latitude": df_att_xcal_data_in_alaska["Lat"],
+        "longitude": df_att_xcal_data_in_alaska["Lon"]
+    })
+    output_alaska_driving_route_file_path = os.path.join(OUTPUT_DIR, "alaska_driving_route.html")
+    plot_driving_route(
+        df_att_gps_data_in_alaska, 
+        center_coordinates=[61.2181, -149.9003],  # Anchorage coordinates
+        output_file_path=output_alaska_driving_route_file_path
+    )
+
+def plot_route_in_hawaii_within_measurements():
+    df_att_xcal_data_in_hawaii = pd.read_csv(os.path.join(HAWAII_ROOT_DIR, "xcal", "att_smart_tput.csv"))
+    df_att_gps_data_in_hawaii = pd.DataFrame({
+        "latitude": df_att_xcal_data_in_hawaii["Lat"],
+        "longitude": df_att_xcal_data_in_hawaii["Lon"]
+    })
+    output_hawaii_driving_route_file_path = os.path.join(OUTPUT_DIR, "hawaii_driving_route.html")
+    plot_driving_route(
+        df_att_gps_data_in_hawaii, 
+        center_coordinates=[20.7943, -156.3319],  # Maui coordinates
+        output_file_path=output_hawaii_driving_route_file_path
+    )
+
 def main():
     if not os.path.exists(OUTPUT_DIR):
         os.makedirs(OUTPUT_DIR)
 
-    att_xcal_data_in_alaska = os.path.join(ALASKA_ROOT_DIR, "xcal", "att_smart_tput.csv")
-    df = pd.read_csv(att_xcal_data_in_alaska)
-    df_att_gps_data_in_alaska = pd.DataFrame({
-        "latitude": df["Lat"],
-        "longitude": df["Lon"]
-    })
-    output_alaska_driving_route_file_path = os.path.join(OUTPUT_DIR, "alaska_driving_route.html")
-    plot_driving_route(df_att_gps_data_in_alaska, output_file_path=output_alaska_driving_route_file_path)
+    plot_route_in_alaska_within_measurements()
+    plot_route_in_hawaii_within_measurements()
+
+    # Just for cross-validation
+    plot_route_in_alaska_with_all_xcal_data()
+    plot_route_in_hawaii_with_all_xcal_data()
+
 # Example usage
 if __name__ == "__main__":
    main()
---
+++ scripts/hawaii_starlink_trip/parse_xcal_tput_to_csv.py
@@ -0,0 +1,82 @@
+from os import path
+import os
+import sys
+import pandas as pd
+
+sys.path.append(path.join(path.dirname(__file__), '../..'))
+
+from scripts.hawaii_starlink_trip.labels import DatasetLabel
+from scripts.hawaii_starlink_trip.separate_dataset import read_dataset
+from scripts.hawaii_starlink_trip.configs import ROOT_DIR, TIMEZONE
+from scripts.constants import DATASET_DIR
+from scripts.utilities.xcal_processing_utils import collect_periods_of_tput_measurements, filter_xcal_logs, read_daily_xcal_data, tag_xcal_logs_with_essential_info
+
+
+XCAL_TIME_STAMP = 'TIME_STAMP'
+XCAL_LOCAL_DATETIME = 'LOCAL_DATETIME'
+XCAL_EVENT_TECHNOLOGY = 'Event Technology'
+XCAL_EVENT_TECHNOLOGY_BAND = 'Event Technology(Band)'
+XCAL_SMART_TPUT_DL = 'Smart Phone Smart Throughput Mobile Network DL Throughput [Mbps]'
+XCAL_SMART_TPUT_UL = 'Smart Phone Smart Throughput Mobile Network UL Throughput [Mbps]'
+
+
+def main():
+    # deal with one location and one operator per time
+    operator = 'att'
+    location = 'hawaii'
+
+    xcal_log_dir = path.join(DATASET_DIR, 'xcal')
+    output_dir = path.join(ROOT_DIR, f'xcal')
+
+    if not path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    dir_list = read_dataset(operator, DatasetLabel.NORMAL.value)
+
+    all_tput_periods = []
+    all_dates = set()
+    for dir in dir_list:
+        # dir is like /path/to/20240621/153752852
+        date = dir.split('/')[-2]
+        all_dates.add(date)
+
+        try:
+          periods_of_tcp_dl = collect_periods_of_tput_measurements(base_dir=dir, protocol='tcp', direction='downlink')
+          periods_of_tcp_ul = collect_periods_of_tput_measurements(base_dir=dir, protocol='tcp', direction='uplink')
+          periods_of_udp_dl = collect_periods_of_tput_measurements(base_dir=dir, protocol='udp', direction='downlink')
+          periods_of_udp_ul = collect_periods_of_tput_measurements(base_dir=dir, protocol='udp', direction='uplink')
+          all_tput_periods.extend(periods_of_tcp_dl + periods_of_tcp_ul + periods_of_udp_dl + periods_of_udp_ul)
+        except Exception as e:
+            raise Exception(f"Failed to collect periods of tput measurements: {str(e)}")
+    print("Successfully collected periods of tput measurements.")
+
+    df_xcal_all_logs = pd.DataFrame()
+    for date in all_dates:
+        try:
+            df_xcal_daily_data = read_daily_xcal_data(base_dir=xcal_log_dir, date=date, location=location, operator=operator)
+            df_xcal_all_logs = pd.concat([df_xcal_all_logs, df_xcal_daily_data])
+            print(f"Successfully read and concatenated xcal data for date: {date}")
+        except Exception as e:
+            print(f"Failed to read or concatenate xcal data for date {date}: {str(e)}")
+
+    try:
+        df_xcal_tput_logs = filter_xcal_logs(df_xcal_all_logs, periods=all_tput_periods)
+        print("Successfully filtered xcal logs.")
+
+        # drop rows with empty dl and ul tput columns
+        df_xcal_tput_logs = df_xcal_tput_logs[df_xcal_tput_logs[XCAL_SMART_TPUT_DL].notna() & df_xcal_tput_logs[XCAL_SMART_TPUT_UL].notna()]
+
+        df_xcal_tput_logs = tag_xcal_logs_with_essential_info(df_xcal_tput_logs, periods=all_tput_periods, timezone=TIMEZONE)
+        
+        # sort by LOCAL_DATETIME
+        df_xcal_tput_logs = df_xcal_tput_logs.sort_values(by='utc_dt')
+        
+        output_file = path.join(output_dir, f'{operator}_smart_tput.csv')
+        df_xcal_tput_logs.to_csv(output_file, index=False)
+        print(f"Successfully saved filtered xcal logs to {output_file}")
+    except Exception as e:
+        print(f"Failed to filter xcal logs: {str(e)}")
+
+
+if __name__ == "__main__":
+    main()
