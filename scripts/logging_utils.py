d2c4c39274514b70173231dd7732902d149bd09f
sizhewang <wang.sizh@northeastern.edu>
2024-10-25 16:13:58 -0400

    perf: update logging utils

--- scripts/logging_utils.py
+++ scripts/logging_utils.py
@@ -18,7 +18,7 @@ def reset_logger(name):
 
 def create_logger(
         name: str, 
-        filename: str,
+        filename: str | None = None,
         formatter: logging.Formatter | None = None,
         filemode: str = 'a'
 ) -> logging.Logger:
@@ -27,17 +27,21 @@ def create_logger(
     if logger.handlers:
         reset_logger(name)
 
-    if not os.path.exists(os.path.dirname(filename)):
-        os.makedirs(os.path.dirname(filename))
-
-    file_handler = logging.FileHandler(filename, mode=filemode)
-    file_handler.setLevel(logging.INFO)
     if formatter is None:
         formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                                      datefmt='%Y-%m-%d %H:%M:%S')
-    file_handler.setFormatter(formatter)
-
-    logger.addHandler(file_handler)
+                                      datefmt='%Y-%m-%dT%H:%M:%S%z')
+    if filename is not None:
+        if not os.path.exists(os.path.dirname(filename)):
+            os.makedirs(os.path.dirname(filename))
+        file_handler = logging.FileHandler(filename, mode=filemode)
+        file_handler.setLevel(logging.INFO)
+        file_handler.setFormatter(formatter)
+        logger.addHandler(file_handler)
+    else:
+        stream_handler = logging.StreamHandler()
+        stream_handler.setLevel(logging.INFO)
+        stream_handler.setFormatter(formatter)
+        logger.addHandler(stream_handler)
 
     return logger
 
