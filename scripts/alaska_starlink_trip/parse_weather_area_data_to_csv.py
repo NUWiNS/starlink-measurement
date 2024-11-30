c836f011984399ae958735f0aad3843a1c2eee1e
sizhewang <wang.sizh@northeastern.edu>
2024-11-30 14:12:16 -0500

    feat(AK): update weather area parser

--- scripts/alaska_starlink_trip/parse_weather_area_data_to_csv.py
+++ scripts/alaska_starlink_trip/parse_weather_area_data_to_csv.py
@@ -6,14 +6,14 @@ import pandas as pd
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '../..'))
 
+from scripts.constants import CommonField
 from scripts.logging_utils import create_logger
 from scripts.weather_area_type_query_utils import parse_weather_area_type_log
+from scripts.alaska_starlink_trip.configs import ROOT_DIR
 
-from scripts.constants import DATASET_DIR
-
-base_dir = os.path.join(DATASET_DIR, 'alaska_starlink_trip/raw/weather_area')
-tmp_data_path = os.path.join(DATASET_DIR, 'alaska_starlink_trip/tmp')
-others_dataset_dir = os.path.join(DATASET_DIR, 'alaska_starlink_trip/others')
+base_dir = os.path.join(ROOT_DIR, 'raw/weather_area')
+tmp_data_path = os.path.join(ROOT_DIR, 'tmp')
+others_dataset_dir = os.path.join(ROOT_DIR, 'others')
 
 logger = create_logger('weather_area_parsing',
                        filename=os.path.join(tmp_data_path, 'parse_weather_area_data_to_csv.log'))
@@ -33,14 +33,14 @@ def main():
         weather_data.extend(result['weather'])
         area_data.extend(result['area'])
 
-    header = ['time', 'value']
+    header = [CommonField.LOCAL_DT, CommonField.UTC_TS, 'value']
     weather_csv_path = os.path.join(others_dataset_dir, 'weather.csv')
-    weather_df = pd.DataFrame(weather_data, columns=header).sort_values(by='time')
+    weather_df = pd.DataFrame(weather_data, columns=header).sort_values(by=CommonField.UTC_TS)
     weather_df.to_csv(weather_csv_path, index=False)
     logger.info(f'Saved weather data to CSV files: {weather_csv_path}')
 
     area_csv_path = os.path.join(others_dataset_dir, 'area.csv')
-    area_df = pd.DataFrame(area_data, columns=header).sort_values(by='time')
+    area_df = pd.DataFrame(area_data, columns=header).sort_values(by=CommonField.UTC_TS)
     area_df.to_csv(area_csv_path, index=False)
     logger.info(f'Saved area data to CSV files: {area_csv_path}')
 
