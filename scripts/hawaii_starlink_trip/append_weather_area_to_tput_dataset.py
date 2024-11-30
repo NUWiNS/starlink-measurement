86cb5c73b1696abd915352dd3fb5905ce27a7479
sizhewang <wang.sizh@northeastern.edu>
2024-11-30 12:53:02 -0500

    feat(HI): re-append area type

--- scripts/hawaii_starlink_trip/append_weather_area_to_tput_dataset.py
+++ scripts/hawaii_starlink_trip/append_weather_area_to_tput_dataset.py
@@ -20,6 +20,12 @@ ping_dir = os.path.join(ROOT_DIR, 'ping')
 logger = create_logger('append_weather_area_to_tput_dataset',
                        filename=os.path.join(tmp_data_path, 'append_weather_area_to_tput_dataset.log'))
 
+def drop_cols_before_appending(tput_df: pd.DataFrame):
+    if 'weather' in tput_df.columns:
+        tput_df.drop(columns=['weather'], inplace=True)
+    if 'area' in tput_df.columns:
+        tput_df.drop(columns=['area'], inplace=True)
+    return tput_df
 
 def append_weather_area_to_xcal_tput_traces(
     xcal_tput_dir: str, 
@@ -33,10 +39,7 @@ def append_weather_area_to_xcal_tput_traces(
         logger.info(f'Processing {tput_csv_file}')
         tput_df = pd.read_csv(tput_csv_file)
 
-        if 'weather' in tput_df.columns:
-            tput_df.drop(columns=['weather'], inplace=True)
-        if 'area' in tput_df.columns:
-            tput_df.drop(columns=['area'], inplace=True)
+        tput_df = drop_cols_before_appending(tput_df)
 
         for idx, row in tput_df.iterrows():
             row_time = pd.to_datetime(row[XcalField.LOCAL_TIME], format="ISO8601")
@@ -48,14 +51,13 @@ def append_weather_area_to_xcal_tput_traces(
 
 
 
-def append_weather_area_to_app_tput_traces(tput_dir: str):
+def append_weather_area_to_app_tput_traces(
+    tput_dir: str, 
+    weather_query_util: TypeIntervalQueryUtil, 
+    area_query_util: TypeIntervalQueryUtil
+):
     all_tput_csv_files = glob.glob(os.path.join(tput_dir, '*.csv'))
     logger.info(f'Found {len(all_tput_csv_files)} throughput CSV files')
-
-    weather_csv_path = os.path.join(others_dir, 'weather.csv')
-    logger.info(f'Loading weather data from {weather_csv_path}')
-    area_csv_path = os.path.join(others_dir, 'area.csv')
-    logger.info(f'Loading area data from {area_csv_path}')
 
     for tput_csv_file in all_tput_csv_files:
         logger.info(f'Processing {tput_csv_file}')
@@ -63,24 +65,22 @@ def append_weather_area_to_app_tput_traces(tput_dir: str):
         tput_df = pd.read_csv(tput_csv_file)
         tput_df['time'] = pd.to_datetime(tput_df['time'], format="ISO8601")
 
-        weather_df = pd.read_csv(weather_csv_path)
-        weather_df['time'] = pd.to_datetime(weather_df['time'], format="ISO8601")
-        weatherIntervalQueryUtil = TypeIntervalQueryUtil(weather_df[['time', 'value']].values.tolist())
-
-        area_df = pd.read_csv(area_csv_path)
-        area_df['time'] = pd.to_datetime(area_df['time'], format="ISO8601")
-        areaIntervalQueryUtil = TypeIntervalQueryUtil(area_df[['time', 'value']].values.tolist())
-
+        tput_df = drop_cols_before_appending(tput_df)
         for idx, row in tput_df.iterrows():
-            tput_df.at[idx, 'weather'] = weatherIntervalQueryUtil.query(row['time'])
-            tput_df.at[idx, 'area'] = areaIntervalQueryUtil.query(row['time'])
+            tput_df.at[idx, 'weather'] = weather_query_util.query(row['time'])
+            tput_df.at[idx, 'area'] = area_query_util.query(row['time'])
 
         tput_df.to_csv(tput_csv_file, index=False)
         logger.info(f'Finished processing {tput_csv_file}, weather and area data appended')
 
 
-def append_weather_area_to_rtt_traces(df: pd.DataFrame, weather_query_util: TypeIntervalQueryUtil, area_query_util: TypeIntervalQueryUtil):
+def append_weather_area_to_rtt_traces(
+        df: pd.DataFrame, 
+        weather_query_util: TypeIntervalQueryUtil, 
+        area_query_util: TypeIntervalQueryUtil
+    ):
     df['time'] = pd.to_datetime(df['time'], format="ISO8601")
+    df = drop_cols_before_appending(df)
     for idx, row in df.iterrows():
         df.at[idx, 'weather'] = weather_query_util.query(row['time'])
         df.at[idx, 'area'] = area_query_util.query(row['time'])
@@ -93,28 +93,31 @@ def main():
     logger.info(f'Loading area data from {area_csv_path}')
     
     weather_df = pd.read_csv(weather_csv_path)
-    weather_df[CommonField.LOCAL_DT] = pd.to_datetime(weather_df[CommonField.LOCAL_DT], format="ISO8601")
-    weatherIntervalQueryUtil = TypeIntervalQueryUtil(weather_df[[CommonField.LOCAL_DT, 'value']].values.tolist())
+    weather_df[CommonField.LOCAL_DT] = pd.to_datetime(weather_df[CommonField.UTC_TS])
+    weather_query_util = TypeIntervalQueryUtil(weather_df[[CommonField.LOCAL_DT, 'value']].values.tolist())
 
     area_df = pd.read_csv(area_csv_path)
-    area_df[CommonField.LOCAL_DT] = pd.to_datetime(area_df[CommonField.LOCAL_DT], format="ISO8601")
-    areaIntervalQueryUtil = TypeIntervalQueryUtil(area_df[[CommonField.LOCAL_DT, 'value']].values.tolist())
+    area_df[CommonField.LOCAL_DT] = pd.to_datetime(area_df[CommonField.UTC_TS])
+    area_query_util = TypeIntervalQueryUtil(area_df[[CommonField.LOCAL_DT, 'value']].values.tolist())
 
-    # append_weather_area_to_app_tput_traces(tput_dir=tput_dir)
-    # append_weather_area_to_app_tput_traces(tput_dir=tput_dir_for_cubic)
-    # append_weather_area_to_app_tput_traces(tput_dir=tput_dir_for_bbr)
-    append_weather_area_to_xcal_tput_traces(
-        xcal_tput_dir=xcal_tput_dir, 
-        weather_query_util=weatherIntervalQueryUtil, 
-        area_query_util=areaIntervalQueryUtil
+    append_weather_area_to_app_tput_traces(
+        tput_dir=tput_dir, 
+        weather_query_util=weather_query_util, 
+        area_query_util=area_query_util
     )
 
-    # for rtt_csv_file in glob.glob(os.path.join(ping_dir, '*_ping.csv')):
-        # logger.info(f'Appending weather and area data to {rtt_csv_file}')
-        # rtt_df = pd.read_csv(rtt_csv_file)
-        # rtt_df = append_weather_area_to_rtt_traces(rtt_df, weatherIntervalQueryUtil, areaIntervalQueryUtil)
-        # rtt_df.to_csv(rtt_csv_file, index=False)
-        # logger.info(f'Finished processing {rtt_csv_file}, weather and area data appended')
+    append_weather_area_to_xcal_tput_traces(
+        xcal_tput_dir=xcal_tput_dir, 
+        weather_query_util=weather_query_util, 
+        area_query_util=area_query_util
+    )
+
+    for rtt_csv_file in glob.glob(os.path.join(ping_dir, '*_ping.csv')):
+        logger.info(f'Appending weather and area data to {rtt_csv_file}')
+        rtt_df = pd.read_csv(rtt_csv_file)
+        rtt_df = append_weather_area_to_rtt_traces(rtt_df, weather_query_util, area_query_util)
+        rtt_df.to_csv(rtt_csv_file, index=False)
+        logger.info(f'Finished processing {rtt_csv_file}, weather and area data appended')
 
 if __name__ == '__main__':
     main()
--- scripts/hawaii_starlink_trip/main.py
+++ scripts/hawaii_starlink_trip/main.py
@@ -12,22 +12,24 @@ from append_weather_area_to_tput_dataset import main as append_weather_area_to_t
 from parse_ping_result_to_csv import main as parse_ping_result_to_csv_main
 from parse_traceroute_data_to_csv import main as parse_traceroute_data_to_csv_main
 from parse_nslookup_data_to_csv import main as parse_nslookup_data_to_csv_main
+from parse_xcal_tput_to_csv import main as parse_xcal_tput_to_csv_main
+from calibrate_weather_area_data import main as calibrate_weather_area_data_main
 
 from plot_cdf_throughput import main as plot_cdf_throughput_main
 from plot_rtt_from_csv import main as plot_rtt_from_csv_main
 from plot_traceroute import main as plot_traceroute_main
 
-
 def parsing():
-    parse_nuttcp_data_to_csv_main()
-    parse_iperf_data_to_csv_main()
+    # parse_nuttcp_data_to_csv_main()
+    # parse_iperf_data_to_csv_main()
+    # parse_xcal_tput_to_csv_main()
+    # parse_ping_result_to_csv_main()
+    # parse_traceroute_data_to_csv_main()
+    # parse_nslookup_data_to_csv_main()
+    
     parse_weather_area_data_to_csv_main()
+    calibrate_weather_area_data_main()
     append_weather_area_to_tput_dataset_main()
-
-    parse_ping_result_to_csv_main()
-    parse_traceroute_data_to_csv_main()
-    parse_nslookup_data_to_csv_main()
-
 
 def plotting():
     plot_cdf_throughput_main()
@@ -36,10 +38,11 @@ def plotting():
 
 
 def main():
-    separate_dataset_main()
+    # separate_dataset_main()
 
     parsing()
-    plotting()
+
+    # plotting()
 
 
 if __name__ == '__main__':
